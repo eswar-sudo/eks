9cbe6735fda3402bed7bc20fc4df6b387caa59a8
eswar-sudo <eswarkumar501@gmail.com>
2025-07-24 15:50:08 +0530

    Create variables.tf

---
+++ modules/nodegroup/variables.tf
@@ -0,0 +1,12 @@
+variable "cluster_name" { type = string }
+variable "node_group_name" { type = string }
+variable "node_role_arn" { type = string }
+variable "subnet_ids" { type = list(string) }
+variable "desired_size" { type = number }
+variable "min_size" { type = number }
+variable "max_size" { type = number }
+variable "instance_types" { type = list(string) }
+variable "capacity_type" { type = string }
+variable "ami_type" { type = string }
+variable "labels" { type = map(string) }
+variable "tags" { type = map(string) }
