a17b89834c29cd0412ce19dfc54c7041d9d8c29c
eswar-sudo <eswarkumar501@gmail.com>
2025-08-11 12:18:58 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -16,6 +16,33 @@ access_config {
 tags = var.tags
 }
 
+resource "aws_eks_access_entry" "root" {
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = "arn:aws:iam::575958559853:root"
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "root_admin" {
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = aws_eks_access_entry.root.principal_arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
+
+resource "aws_eks_access_policy_association" "root_cluster_admin" {
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = aws_eks_access_entry.root.principal_arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
+
+
 resource "aws_iam_role" "eks_cluster_role" {
   name = "${var.cluster_name}-cluster-role"
   assume_role_policy = jsonencode({
