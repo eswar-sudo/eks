38e3dadfc4d99a6db16fe4d229db687b46239bb2
eswar-sudo <eswarkumar501@gmail.com>
2025-08-13 13:19:21 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -10,10 +10,10 @@ resource "aws_eks_cluster" "this" {
     endpoint_public_access  = false
   }
 
-#access_config {
- #   authentication_mode                         = "API_AND_CONFIG_MAP"
-  #  bootstrap_cluster_creator_admin_permissions = true
-  #}
+access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
 
 tags = var.tags
 }
