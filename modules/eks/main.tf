8a77b9dfd91dcb75facbb9b687477de0f413a1dc
eswar-sudo <eswarkumar501@gmail.com>
2025-08-21 22:56:38 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -5,7 +5,7 @@ resource "aws_eks_cluster" "this" {
   version  = var.cluster_version
 
   vpc_config {
-    subnet_ids = var.subnet_ids
+    subnet_ids = var.private_subnet_ids
     endpoint_private_access = true
     endpoint_public_access  = false
   }
