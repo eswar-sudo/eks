19aacbe3e0f38af3851446fb1733c44b31514f5a
eswar-sudo <eswarkumar501@gmail.com>
2025-08-21 22:57:48 +0530

    Update variables.tf

--- modules/nodegroup/variables.tf
+++ modules/nodegroup/variables.tf
@@ -1,7 +1,7 @@
 variable "cluster_name" { type = string }
 variable "node_group_name" { type = string }
 variable "node_role_arn" { type = string }
-variable "subnet_ids" { type = list(string) }
+variable "private_subnet_ids" { type = list(string) }
 variable "desired_size" { type = number }
 variable "min_size" { type = number }
 variable "max_size" { type = number }
