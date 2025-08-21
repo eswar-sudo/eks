d6ae1c12beb866184910151c4886dc0a9d3cdd93
eswar-sudo <eswarkumar501@gmail.com>
2025-08-21 22:57:03 +0530

    Update variables.tf

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,7 +1,7 @@
 variable "cluster_name" { type = string }
 variable "region" { type = string }
 variable "vpc_id" { type = string }
-variable "subnet_ids" { type = list(string) }
+variable "private_subnet_ids" { type = list(string) }
 variable "cluster_version" { type = string }
 variable "tags" { type = map(string) }
 variable "vpc_cni_version" {
